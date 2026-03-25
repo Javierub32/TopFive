41ded43d3aef6ebaa2379cd91f10e8539b5802f4
Paula <paulafj2108@gmail.com>
2026-03-25 13:36:22 +0100

    Change review text size at home

--- src/Home/components/RenderResource.tsx
+++ src/Home/components/RenderResource.tsx
@@ -101,13 +101,13 @@ export default function ActivityItem({ item }: { item: Activity }) {
               </TouchableOpacity>
 
               {/* Detalles del Recurso */}
-              <View className="flex-col flex-1">
+              <View className="flex-col flex-1 ">
                 <View className="flex-row justify-between items-start">
                   <View className='flex-1'>
                     <Text className="font-bold text-base leading-tight mr-2 mb-2" style={{ color: colors.primaryText }}>{item.titulo}</Text>
                     {/* Calificación */}
                     {rating > 0 && (
-                      <View className="flex-row items-center gap-1">
+                      <View className="flex-row items-center gap-1 mt-1">
                         {[1, 2, 3, 4, 5].map((star) => {
                           const rating = item.calificacion || 0;
                           let iconName: any = "star";
@@ -139,11 +139,12 @@ export default function ActivityItem({ item }: { item: Activity }) {
                   </View>
                 </View>
                 {/* Reseña */}
-                <Text style={{ color: colors.secondaryText}}>
-                  <Text className="italic leading-relaxed">{displayedDescription}</Text>
+                <Text style={{ color: colors.secondaryText}}
+                  className="mt-2 flex-row items-center">
+                  <Text className="italic leading-relaxed text-md">{displayedDescription}</Text>
                   {shouldTruncate && (
                     <Text
-                      className="font-bold"
+                      className="font-bold text-md"
                       style={{ color: colors.primary }}
                       onPress={() => setIsExpanded(!isExpanded)}>
                       {isExpanded ? ' Leer menos' : 'Leer más'}
