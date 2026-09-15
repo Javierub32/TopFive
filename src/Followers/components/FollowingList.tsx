37b3f07e81dfc788bd42796efd30511d742e52f2
Paula <paulafj2108@gmail.com>
2026-09-15 12:11:40 +0200

    feature: added user search on followers and following page

--- src/Followers/components/FollowingList.tsx
+++ src/Followers/components/FollowingList.tsx
@@ -5,18 +5,36 @@ import { LoadingIndicator } from "components/LoadingIndicator";
 import { useFollowing } from "../hooks/useFollowing";
 import { ScalableCancelIcon } from "components/Icons";
 import { useTheme } from "context/ThemeContext";
+import { useCollection } from "context/CollectionContext";
+import { useMemo, useState } from "react";
+import { UserSearchBar } from "@/Search/components/UserSearchBar";
 
 export default function FollowingList() {
 	const {username } = useLocalSearchParams<{ username: string }>();
 	const { loading, following, handleRemovePress, ownList } = useFollowing(username);
 	const { colors } = useTheme();
+  const { isSearchVisible, toggleSearch } = useCollection();
+	const [busqueda, setBusqueda] = useState('');
 
+	//Con esto, filtramos de la lista de los seguidos y se actualiza directamente
+	const userFiltered = useMemo(() => {
+		if(!busqueda.trim()) return following;
+		return following.filter(user => user.username.toLowerCase().includes(busqueda.toLowerCase()));
+	}, [busqueda, following]);
 	if (loading) {
 		return <LoadingIndicator />;
 	}
 	return (
+		<>
+		<View className="mt-4 px-4">
+		<UserSearchBar
+			value={busqueda}
+			onChangeText={setBusqueda}
+			onSearch={() => {}}
+		/>
+		</View>
 		<FlatList 
-			data={following}
+			data={userFiltered}
 			keyExtractor={(user) => user.id.toString()}
 			renderItem={({ item }) => 
 			<View className="flex flex-row items-center space-x-4 pl-4 pr-8 py-3">
@@ -37,5 +55,6 @@ export default function FollowingList() {
 			contentContainerStyle={{ paddingBottom: 20 }}
 			showsVerticalScrollIndicator={false}
 		/>
+		</>
 	);
 } 
